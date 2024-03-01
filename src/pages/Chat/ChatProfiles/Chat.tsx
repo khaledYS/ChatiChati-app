3fdf3a204736798edc9e1af32b9266aa8be1dc83
ammar <ammar@gmail.com>
2024-03-01 05:39:11 -0800

    added shadow to the current chat

--- src/pages/Chat/ChatProfiles/Chat.tsx
+++ src/pages/Chat/ChatProfiles/Chat.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Dispatch, SetStateAction, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 
 interface Props {
@@ -9,12 +9,16 @@ interface Props {
         email: string;
     };
     chatId: string;
+    currentChat: string;
+    setCurrentChat: Dispatch<SetStateAction<string>>
 }
 
 function Chat(props: Props) {
     const { messager, chatId } = props;
     return (
-        <Link to={chatId} style={{ "boxShadow": "rgb(42, 42, 42) 0px 0px 7px -7px inset, rgb(42, 42, 42) 0px 0px 3px -1px"}} className='w-full px-2 py-2 w bg-[#e3e3e3] hover:bg-[#c3c3c3] transition-all hover:cursor-pointer rounded-md mt-2 flex overflow-hidden'>
+        <Link to={chatId} onClick={()=>{
+            props.setCurrentChat(chatId);
+        }} style={{ "boxShadow": "rgb(42, 42, 42) 0px 0px 7px -7px inset, rgb(42, 42, 42) 0px 0px 3px -1px"}} className={'w-full px-2 py-2 w bg-[#e3e3e3] hover:bg-[#c3c3c3] transition-all hover:cursor-pointer rounded-md mt-2 flex overflow-hidden ' + (props.currentChat === chatId ? 'bg-[#c3c3c3]' : '')}>
             <img width={50} src={`https://api.dicebear.com/7.x/initials/png?seed=${messager.username}`} alt={`user @${messager.username} avatar`}  className='block rounded-full mr-2  '/>
             <div >
                 <h1 className='text-lg font-medium'> {messager.name} | @{messager.username} </h1>
--- src/pages/Chat/ChatProfiles/Chats.tsx
+++ src/pages/Chat/ChatProfiles/Chats.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ChatsFromRequest } from "./index";
 import { useAuth } from "@/hooks/useAuth";
 import Chat from "./Chat";
@@ -10,13 +10,14 @@ interface Props {
 function Chats(props: Props) {
     const { chats } = props;
     const { user } = useAuth();
+    const [currentChat, setCurrentChat] = useState("")
     console.log(chats)
     return (
         <div className="w-full px-2 py-2">
             {chats &&
                 chats.map((chat, ind) => {
                     const chater = chat.messagers.filter((val) => val.username !== user.username);
-                    return <Chat key={ind} messager={{...chater[0]}} chatId={chat.chatId} />;
+                    return <Chat key={ind} messager={{...chater[0]}} chatId={chat.chatId} currentChat={currentChat} setCurrentChat={setCurrentChat} />;
                 })}
         </div>
     );
